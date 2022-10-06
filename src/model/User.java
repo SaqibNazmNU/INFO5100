4f64f2486b8197a273a44e25d9fe57bcf5a43a26
unknown <nazm.s@northeastern.edu>
2022-10-06 13:37:30 -0400

    initial commit
    initial commit

---
+++ src/model/User.java
@@ -0,0 +1,186 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author saqibnazm
+ */
+public class User {
+    
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    private String age;
+    private String telNumber;
+    private String email;
+    private String streetLineOne;
+    private String streetLineTwo;
+    private String city;
+    private String country;
+    private String affiliation;
+    private String major;
+    private String career;
+    private String degreeOne;
+    private String degreeStart;
+    private String degreeEnd;
+    private String degreeTwo;
+    private String degreeTwoStart;
+    private String degreeTwoEnd;
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+    
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+    
+    public String getTelNumber() {
+        return telNumber;
+    }
+
+    public void setTelNumber(String telNumber) {
+        this.telNumber = telNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStreetLineOne() {
+        return streetLineOne;
+    }
+
+    public void setStreetLineOne(String streetLineOne) {
+        this.streetLineOne = streetLineOne;
+    }
+
+    public String getStreetLineTwo() {
+        return streetLineTwo;
+    }
+
+    public void setStreetLineTwo(String streetLineTwo) {
+        this.streetLineTwo = streetLineTwo;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getAffiliation() {
+        return affiliation;
+    }
+
+    public void setAffiliation(String affiliation) {
+        this.affiliation = affiliation;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getCareer() {
+        return career;
+    }
+
+    public void setCareer(String career) {
+        this.career = career;
+    }
+
+    public String getDegreeOne() {
+        return degreeOne;
+    }
+
+    public void setDegreeOne(String degreeOne) {
+        this.degreeOne = degreeOne;
+    }
+
+    public String getDegreeStart() {
+        return degreeStart;
+    }
+
+    public void setDegreeStart(String degreeStart) {
+        this.degreeStart = degreeStart;
+    }
+
+    public String getDegreeEnd() {
+        return degreeEnd;
+    }
+
+    public void setDegreeEnd(String degreeEnd) {
+        this.degreeEnd = degreeEnd;
+    }
+
+    public String getDegreeTwo() {
+        return degreeTwo;
+    }
+
+    public void setDegreeTwo(String degreeTwo) {
+        this.degreeTwo = degreeTwo;
+    }
+
+    public String getDegreeTwoStart() {
+        return degreeTwoStart;
+    }
+
+    public void setDegreeTwoStart(String degreeTwoStart) {
+        this.degreeTwoStart = degreeTwoStart;
+    }
+
+    public String getDegreeTwoEnd() {
+        return degreeTwoEnd;
+    }
+
+    public void setDegreeTwoEnd(String degreeTwoEnd) {
+        this.degreeTwoEnd = degreeTwoEnd;
+    }
+    
+}
